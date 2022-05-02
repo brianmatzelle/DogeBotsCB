5e75ba949d1cd1506911332d30d98b7569cc1f3c
Brian <bmatzel1@binghamton.edu>
2022-05-02 02:43:27 -0400

    configured for an hour

--- firstBot/bot.py
+++ firstBot/bot.py
@@ -2,7 +2,7 @@ from tempfile import TemporaryFile
 import cbpro
 import time
 from numpy import double
-import cbpro
+from datetime import datetime
 
 data = open('passphrase', 'r').read().splitlines()
 public = data[0]
@@ -12,6 +12,7 @@ authClient = cbpro.AuthenticatedClient(public, secret, passphrase)
 dogeAccountInfo = authClient.get_account('334285d3-d4fb-4de1-aa39-e7fd0c3078b6')
 getBalance = lambda: double(dogeAccountInfo['balance'])
 price = lambda: float(authClient.get_product_ticker(product_id="DOGE-USD")['price'])
+print(f"Balance: {getBalance()} DOGE")
 
 # try to trade every __ seconds
 REFRESH_EVERY = 20
@@ -29,43 +30,42 @@ MINIMUM = 3.8
 getMaxAvail = lambda: getBalance()
 
 # TRADE CONFIGS, ADJUST THESE TO YOUR LIKING
-BUY_AT_PERCENTAGE = .98
-SELL_AT_PERCENTAGE = 1.02
+BUY_AT_PERCENTAGE = .992
+SELL_AT_PERCENTAGE = 1.008
 newPriceEvery = HOUR
 
 def buyActionHighTrades(BUY_AMOUNT):
     buyAction = lambda: authClient.place_market_order(size=BUY_AMOUNT, side="buy", product_id="DOGE-USD")
     try:                                                      # high amount of trades
-        if buyAction()['message'] == 'Insufficient funds':
-            return False
-    except Exception:
         print(buyAction())
         print(f"New balance: {getBalance()}")
         return True
+    except Exception:
+        return False
 
 def buyActionLowTrades(BUY_AMOUNT):
     buyAction = lambda: authClient.place_market_order(size=BUY_AMOUNT, side="buy", product_id="DOGE-USD")
     try:
-        if buyAction()['message'] == 'Insufficient funds':
-            return False
-    except Exception:
         print(buyAction())
         print(f"New balance: {getBalance()}")
         return True
+    except Exception:
+        return False
 
-def sellActionHighTrades(SELL_AMOUNT):
+def sellActionLowTrades(SELL_AMOUNT):
     sellAction = lambda: authClient.place_market_order(size=SELL_AMOUNT, side="sell", product_id="DOGE-USD")
     try:                                                      # high amount of trades
-        if sellAction()['message'] == 'Insufficient funds':
-            return False
-    except Exception:
         print(sellAction())
         print(f"New balance: {getBalance()}")
         return True
+    except Exception:
+        return False
 
 while True:
+    print(f"Time started: {datetime.now()}")
     stallCounter = 0
     timeCounter = 0
+    extraStallCounter = 0
     SELL_PRICE = price() * SELL_AT_PERCENTAGE
     BUY_PRICE = price() * BUY_AT_PERCENTAGE
     print("Current price: " + authClient.get_product_ticker(product_id="DOGE-USD")['price'])
@@ -74,14 +74,11 @@ while True:
     while True:
         if price() <= BUY_PRICE:
             print(f"Buying DOGE! Price fell to {price()} ...")
-            # stall the counter if the threshold is crossed
-            # timeCounter -= REFRESH_EVERY - (REFRESH_EVERY / 4)
-            # stallCounter += REFRESH_EVERY - (REFRESH_EVERY / 4)
             if buyActionLowTrades(getBalance() / 2) == True:
-                # refresh buy price to account for resistance
-                BUY_PRICE = price() * .996
-                timeCounter -= newPriceEvery / 4
-                stallCounter += newPriceEvery / 4
+                # refresh buy/sell price to account for resistance
+                SELL_PRICE = price() * 1.003
+                BUY_PRICE = price() * .997
+                extraStallCounter = -1
                 
             else:
                 print("Insufficient funds...")
@@ -90,13 +87,11 @@ while True:
 
         elif price() >= SELL_PRICE:
             print(f"Selling DOGE! Price rose to {price()} ...")
-            # stall the counter if the threshold is crossed
-            
-            if sellActionHighTrades(getBalance() / 2) == True:
-                # refresh sell price to account for resistance
-                SELL_PRICE = price() * 1.004
-                timeCounter -= newPriceEvery / 4
-                stallCounter += newPriceEvery / 4
+            if sellActionLowTrades(getBalance() / 2) == True:
+                # refresh buy/sell price to account for resistance
+                SELL_PRICE = price() * 1.003
+                BUY_PRICE = price() * .997
+                extraStallCounter = -1
 
             else:
                 print("Insufficient funds...")
@@ -106,6 +101,28 @@ while True:
         # refresh
         time.sleep(REFRESH_EVERY)
         timeCounter += REFRESH_EVERY
+        if ((price() - BUY_PRICE) / price()) * 100 <= .10 and extraStallCounter < 3:
+            timeCounter -= newPriceEvery / 12                           # give extra 5 min if 10% away from buy price
+            stallCounter += newPriceEvery / 12
+            extraStallCounter += 1
+
+        elif ((price() - BUY_PRICE) / price()) * 100 <= .15 and extraStallCounter < 2:            # give extra 10 min if 15% away from buy price
+            timeCounter -= newPriceEvery / 12
+            stallCounter += newPriceEvery / 12
+            extraStallCounter += 1
+
+        elif ((SELL_PRICE - price()) / price()) * 100 <= .10 and extraStallCounter < 3:
+            timeCounter -= newPriceEvery / 12
+            stallCounter += newPriceEvery / 12
+            extraStallCounter += 1
+
+        elif ((SELL_PRICE - price()) / price()) * 100 <= .15 and extraStallCounter < 2:
+            timeCounter -= newPriceEvery / 12
+            stallCounter += newPriceEvery / 12
+            extraStallCounter += 1
+            
+        if timeCounter == newPriceEvery / 2:
+            print(f"{timeCounter / 60} minutes has passed, continuing...")
         if (timeCounter > newPriceEvery):
             print(f"{(newPriceEvery + stallCounter) / 60} minutes has passed... Stalled time: {stallCounter / 60} minutes")
             break
